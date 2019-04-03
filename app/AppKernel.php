346b1275b2bbb1e36c99381091a4411066a56d19
Vitaliy <gapon007@ukr.net>
2019-04-03 12:31:58 +0300

    Install knp menu Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
 
         ];
 
