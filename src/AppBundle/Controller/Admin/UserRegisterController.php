8ae01ef619731dede1bd73216f65458a8a1f2e3b
Vitaliy <gapon007@ukr.net>
2019-04-04 18:02:46 +0300

    Realize Registration

---
+++ src/AppBundle/Controller/Admin/UserRegisterController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pro
+ * Date: 2019-04-04
+ * Time: 17:24
+ */
+
+namespace AppBundle\Controller\Admin;
+
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserRegistrationForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction(Request $request)
+    {
+
+        $form = $this->createForm(UserRegistrationForm::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            /**
+             * @var User $user
+             */
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Welcome ' . $user->getEmail());
+
+            return $this->redirectToRoute('homepage');
+
+        }
+
+        return $this->render('authorizate/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+
+    }
+
+}
